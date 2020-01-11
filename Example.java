dd5fdce3296122007167737d9bdc68755d477869
Shapiro Igor <shapiroigor96@gmail.com>
2020-01-11 13:45:02 +0400

    second program

--- Example.java
+++ Example.java
@@ -1,7 +1,17 @@
 class Example
 {
-	public static void main(String args[]) 
+	public static void main(String args[])
 	{
-		System.out.println("Java правит Интернетом!");
+		int var1;
+		int var2;
+		
+		var1 = 1024;
+		
+		System.out.println("Переменная var1 содержит " + var1);
+		
+		var2 = var1 / 2;
+		
+		System.out.print("Переменная var2 содержит var 1 / 2: ");
+		System.out.println(var2);
 	}
 }
