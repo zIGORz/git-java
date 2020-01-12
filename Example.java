7e507e99d70df8922fe3e71b90332be47d442a76
Shapiro Igor <shapiroigor96@gmail.com>
2020-01-12 16:32:34 +0400

    BigDick

--- Example.java
+++ Example.java
@@ -2,16 +2,14 @@ class Example
 {
 	public static void main(String args[])
 	{
-		int var1;
-		int var2;
-		
-		var1 = 1024;
-		
-		System.out.println("Переменная var1 содержит " + var1);
-		
-		var2 = var1 / 2;
-		
-		System.out.print("Переменная var2 содержит var 1 / 2: ");
-		System.out.println(var2);
+		int i;
+		int a;
+		for(i = 0; i < 20; i++)
+		{
+			a = (int) (Math.random() * 21);
+			if ((a % 3 == 0) && (a % 5 == 0)) System.out.println("Число " + a + " is a BigDick");
+			else if (a % 3 == 0) System.out.println("Число " + a + " is a Big");
+			else if (a % 5 == 0) System.out.println("Число " + a + " is a Dick");
+		}
 	}
 }
